9eaf7ffb185c085c4bbb298d2668535ae878e7a8
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-01 21:54:01 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,13 +11,6 @@ client.on('message', msg => {
        let target = msg.mentions.users.first();
        msg.target.sendMessage(msg.content.substr(p.length + "msg ".length) + msg.author.nickname);
 }
-  if(msg.content === (p + "help")) {
-       msg.channel.sendMessage("Help your ass man im a pre-matured bots");
-}
-  
-  if(msg.content === (p + "info")) {
-       msg.channel.sendMessage("Bello anello, my commands available are only : hi and help.");
-}
   
   if(msg.content === (p + "hi")) {
        msg.channel.sendMessage("hi bitch");
