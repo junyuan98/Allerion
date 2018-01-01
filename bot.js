a747a530667af077236f2e984eb7fc5aee46eb99
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-01 22:02:40 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ const p = "a!";
 client.on('message', msg => {
   if(msg.content === (p + "msg ")) {
        let target = msg.mentions.users.first();
-       msg.target.sendMessage(msg.content.substr(p.length + "msg ".length) + msg.author.nickname);
+       msg.target.sendMessage(msg.content.substr(p.length + "msg ".length) + message.author.send("lol"););
 }
   
   if(msg.content === (p + "info")) {
