8fe6085a93e98695ab825e02cd86490c8f73654f
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-01 22:06:10 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,10 +7,7 @@ client.on('ready', () => {
 
 const p = "a!";
 client.on('message', msg => {
-  if (msg.content.startsWith(p + "test ")) {
-        msg.channel.sendMessage("Well Id be + msg.content.substr("test ".length + p.length) + "! ");
-}
-  
+
   if(msg.content === (p + "info")) {
        msg.channel.sendMessage("My prefix is a! and my commands have only 2: help and hi.");
 }
