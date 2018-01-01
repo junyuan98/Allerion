8e3708e10c9b6eb65dded3b4be7408be6b88110d
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-01 21:56:05 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,6 +12,14 @@ client.on('message', msg => {
        msg.target.sendMessage(msg.content.substr(p.length + "msg ".length) + msg.author.nickname);
 }
   
+  if(msg.content === (p + "info")) {
+       msg.channel.sendMessage("My prefix is a! and my commands have only 2: help and hi.");
+}
+  
+  if(msg.content === (p + "help")) {
+       msg.channel.sendMessage("help your ass man im a prematured teenage bot");
+}
+  
   if(msg.content === (p + "hi")) {
        msg.channel.sendMessage("hi bitch");
 }
