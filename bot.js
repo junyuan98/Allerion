ddd94dfc6296c1a2d92276287eb66e7045cc24b3
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-01 22:40:07 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,18 +10,13 @@ client.on('message', msg => {
   if (msg.content.startsWith(p + "repeat ") ) {
         msg.channel.sendMessage("You just typed "+ msg.content.substr("repeat ".length + p.length) + "! ");
 }
-  
-  if (msg.content === (p + "random")) {
-        var x = Math.floor((Math.random() * 10 + 1 );
-        msg.channel.sendMessage("You just randomed "+ x + "! ");
-}
-    
+      
   if(msg.content === (p + "info")) {
        msg.channel.sendMessage("My prefix is a! and my commands are: repeat, help and hi.");
 }
   
   if(msg.content === (p + "help")) {
-       msg.channel.sendMessage("help your ass man im a prematured teenage bot");
+       msg.channel.sendMessage("help your ass man im a prematured bot, go to info for more info");
 }
   
   if(msg.content === (p + "hi")) {
