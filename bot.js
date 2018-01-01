f32a01f4a675fc5e84fae6e70bb4f0bee8468fc0
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-01 20:46:17 +0800

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.username}!`);
+});
+
+const p = "a=";
+client.on('message', msg => {
+	if(msg.content === (p + "hi ")) {
+       msg.channel.sendMessage("Hi bitch.");
+}
+});
+
+client.login(process.env.BOT_TOKEN);
