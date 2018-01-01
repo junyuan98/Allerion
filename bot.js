b5586e32822117e9ec35352d9fd21f59828456e3
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-02 00:30:17 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ client.on('message', msg => {
   }
     if (msg.content === (p + "checkavatar")){
     // Send the user's avatar URL
-    message.reply(message.author.avatarURL);
+    msg.reply(msg.author.avatarURL);
     }
   if(msg.content === (p + "info")) {
        msg.channel.sendMessage("My prefix is a! and my commands are: checkavatar, help, hi, repeat");
