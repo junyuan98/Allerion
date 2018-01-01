b0432f2ae44977ff5a63e992f6465f77dc93b6c4
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-02 00:56:20 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,7 +22,7 @@ client.on('message', msg => {
   
   if(msg.content === (p + "info")) {
        msg.channel.sendMessage("My prefix is a! and my commands are: checkavatar, help, hi, msg, trigger, repeat");
-       msg.channel.sendMessage("test5");
+       msg.channel.sendMessage("test6");
 }
   
   if(msg.content === (p + "trigger")) {
