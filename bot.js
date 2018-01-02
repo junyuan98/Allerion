e61986d10480c21401be2a772b7ba3af878633c1
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-02 20:40:52 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,16 +15,20 @@ client.on('message', msg => {
     } 
 	
 	if (msg.content === ( p + "checkversion" )) {
-		msg.channel.sendMessage("Allerion version 0.0.1 - Alpha-alpha phase");
+		msg.channel.sendMessage("Allerion version 0.0.1 - Revived from dead");
 	}
 
 	if (msg.content === ( p + "help" )) {
 		msg.channel.sendMessage("My prefix is a! and my available commands are:");
-		msg.channel.sendMessage("checkversion, checkavatar, random, randomquote, repeat, ping.");
+		msg.channel.sendMessage("checkversion, checkavatar, ping.");
 	}
 	
 	if (msg.content === (p + "checkavatar")) {
 		msg.reply(msg.author.avatarURL);
+	}
+	
+	if (msg.content === ( p + "ping" )) {
+		msg.channel.sendMessage( msg.author + ", Pong! My ping is " client.ping + "ms." );
 	}
 	
 });
