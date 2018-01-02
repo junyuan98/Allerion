811f9125bb7ce1ccd586afd41d21d0a0c792f171
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-02 18:36:42 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const p = "a!";
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.username}!`);
@@ -10,39 +9,40 @@ function randomQuote() {
 	return [Math.floor(Math.random() * 10 + 1)];
 };
 
+const p = "a!";
 client.on('message', msg => {
 	
-  if (msg.content.startsWith(p + "repeat ") ) {
-        msg.channel.sendMessage("You just typed "+ msg.content.substr("repeat ".length + p.length) + "! ");
-  }
+	if (msg.content.startsWith(p + "repeat ") ) {
+		msg.channel.sendMessage("You just typed "+ msg.content.substr("repeat ".length + p.length) + "! ");
+	}
 	
-    if (msg.content === (p + "checkavatar")){
-    // Send the user's avatar URL
-    msg.reply(msg.author.avatarURL);
-    }
-
-  if(msg.content === (p + "trigger ")) {
-    let member = msg.mentions.members.first();
-	  if(!member)
-		  return msg.reply('Please tag a valid member.');
-	  else { msg.channel.sendMessage( ${member.user.tag} ", you are weak`"); }
-  }
+	if (msg.content === (p + "checkavatar")){
+		// Send the user's avatar URL
+		msg.reply(msg.author.avatarURL);
+	}
+	
+	if(msg.content === (p + "trigger ")) {
+		let member = msg.mentions.members.first();
+		if(!member) {
+			return msg.reply('Please tag a valid member.'); }
+		else { msg.channel.sendMessage( ${member.user.tag} ", you are weak`"); }
+	}
   
-  if(msg.content === (p + "info")) {
-       msg.channel.sendMessage("My prefix is a! and my commands are: hi, help, ping, checkavatar, repeat, random, trigger(beta)");  
-  }
-  
-  if(msg.content === (p + "help")) {
-       msg.channel.sendMessage("`help your ass man im a prematured bot, go to info for more info`");
-  }
-  
-  if(msg.content === (p + "ping")) {
-    msg.channel.sendMessage( msg.author + ", Pong! My ping is " + client.ping + "ms.");
-  }
+	if(msg.content === (p + "info")) {
+		msg.channel.sendMessage("My prefix is a! and my commands are: hi, help, ping, checkavatar, repeat, random, trigger(beta)");  
+	}
  
-  if(msg.content === (p + "hi")) {
-       msg.channel.sendMessage("hi bitch");
-}
+	if(msg.content === (p + "help")) {
+		msg.channel.sendMessage("`help your ass man im a prematured bot, go to info for more info`");
+	}
+  
+	if(msg.content === (p + "ping")) {
+		msg.channel.sendMessage( msg.author + ", Pong! My ping is " + client.ping + "ms.");
+	}
+ 
+	if(msg.content === (p + "hi")) {
+		msg.channel.sendMessage("hi bitch");
+	}
 });
 
 client.login(process.env.BOT_TOKEN);
