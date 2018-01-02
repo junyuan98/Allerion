2192067146b0ca0b16dd6ecebd7747eabb1b1219
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-02 21:27:34 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 	client.user.setGame(`on Alpha testing`);
 });
 
-function randomnumber(){
-	return [Math.floor(Math.random() * 10 + 1)];
+function randomnumber(var y){
+	return [Math.floor(Math.random() * y + 1)];
 };
 
 const p = "a!";
@@ -16,13 +16,13 @@ client.on('message', msg => {
 	if (msg.author.bot) return;
 	
 	if (msg.content === ( p + "checkversion" )) {
-		msg.channel.sendMessage("Allerion version 0.0.2.4 - Revived from dead");
-		msg.channel.sendMessage("`Randomquote function is done(i guess)`");
+		msg.channel.sendMessage("Allerion version 0.0.3.0 - Recovery from dead");
+		msg.channel.sendMessage("`Sing is added, randomquote changed to selftrigger, random in beta phase`");
 	}
 
 	if (msg.content === ( p + "help" )) {
 		msg.channel.sendMessage("My prefix is a! and my available commands are:");
-		msg.channel.sendMessage("checkversion, checkavatar, random, randomquote, ping.");
+		msg.channel.sendMessage("checkversion, checkavatar, random(beta), selftrigger, sing, ping.");
 	}
 	
 	if (msg.content === (p + "checkavatar")) {
@@ -34,19 +34,34 @@ client.on('message', msg => {
 	}
 	
 	if (msg.content === (p + "random")) {
-		var x = randomnumber();
+		var x = randomnumber(suffix);
 		msg.channel.sendMessage("You have randomed " + x );
 	}
 	
-	if (msg.content === ( p + "randomquote" )) {
+	if (msg.content === ( p + "selftrigger" )) {
 		var textArray = [
 			'Fuck off',
 			'Fuck you',
 			'Shut up',
 			'Diam 7 pls',
-			'You are shit'];
+			'You are shit'
+		];
 		var Quote = Math.floor(Math.random() * textArray.length);
 		msg.reply(`${textArray[Quote]}`);
+	}
+	
+	if (msg.content === ( p + "sing" )) {
+		var songLyrics = [
+			'Havana oh nana, half of my heart is in havana na na na',
+			'Is it too late now to say sorry',
+			'2+2 IS 4, MINUS 1 THATS 3 QUICK MATHS',
+			'BOOM BOOM POW GONNA GET GET',
+			'Despacito, nlskdnflkjkljfklsrfkljdlk burito',
+			'You just want attention, you dont want my heart',
+			'We were just kids when we fell , in, love',
+		];
+		var number = Math.floor(Math.random() * songLyrics.length);
+		msg.channel.sendMessage(`${songLyrics[number]}`);
 	}
 	
 });
