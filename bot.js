d27cecf04397b638722e57617e65482aef5abb77
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-02 20:06:19 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,60 +1,58 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.on('ready', () => {
+client.on('ready',() => {
 	console.log(`Logged in as ${client.user.username}!`);
 	client.user.setGame(`Alpha phase testing`);
 });
 
-function random() {
-	return [Math.floor(Math.random() * 10 + 1];
+function randomnumber(){
+	return [Math.floor(Math.random() * 10 + 1)];
 };
 
 const p = "a!";
-			   
-client.on('message', msg => {
-	
-	// It's good practice to ignore other bots. This also makes your bot ignore itself
-	// and not get into a spam loop (we call that "botception").
-	if(msg.author.bot) return;
-		
 
+client.on('message', msg => {
+	if (msg.author.bot) return;
 	
-	if(msg.content === (p + "random")) {
-		var x = random();
-		msg.channel.sendMessage("You have randomed " + x);
+	if (msg.content === ( p + "checkversion" )) {
+		msg.channel.sendMessage("Allerion version 0.0.1 - Alpha-alpha phase");
 	}
 	
-	if (msg.content.startsWith(p + "repeat ") ) {
-		msg.channel.sendMessage("You just typed "+ msg.content.substr("repeat ".length + p.length) + "! ");
+	if (msg.content === ( p + "help" )) {
+		msg.channel.sendMessage("My prefix is a! and my available commands are:");
+		msg.channel.sendMessage("checkversion, checkavatar, random, randomquote, repeat, ping.");
 	}
 	
-	if (msg.content === (p + "checkavatar")){
-		// Send the user's avatar URL
+	if (msg.content === (p + "random")) {
+		var x = randomnumber();
+		msg.channel.sendMessage("You have randomed " x );
+	}
+	
+	if (msg.content === (p + "checkavatar")) {
 		msg.reply(msg.author.avatarURL);
 	}
 	
-	if(msg.content === (p + "trigger ")) {
-		let member = msg.mentions.members.first();
-		if(!member) { msg.reply('Please tag a valid member.'); }
-		else { msg.channel.sendMessage( " ${member.user.tag} , you are weak`"); }
+	if (msg.content === ( p + "ping" )) {
+		msg.channel.sendMessage( msg.author + ", Pong! My ping is " client.ping + "ms." );
 	}
-  
-	if(msg.content === (p + "info")) {
-		msg.channel.sendMessage("My prefix is a! and my commands are: hi, help, ping, checkavatar, repeat, randomquote, trigger(beta)");  
+
+	if (msg.content === ( p + "repeat" )) {
+		msg.channel.sendMessage("You just typed "+ msg.content.substr("repeat ".length + p.length) + "! ");
 	}
- 
-	if(msg.content === (p + "help")) {
-		msg.channel.sendMessage("`help your ass man im a prematured bot, go to info for more info`");
+	
+	if (msg.content === ( p + "randomquote" )) {
+		var textArray = [
+			'Fuck off',
+			'Fuck you',
+			'Shut up',
+			'Diam 7 pls',
+			'You are shit'
+			];
+		var Quote = Math.floor(Math.random() * textArray.length);
+		msg.reply(`${testArray[Quote]}`);
 	}
-  
-	if(msg.content === (p + "ping")) {
-		msg.channel.sendMessage( msg.author + ", Pong! My ping is " + client.ping + "ms.");
-	}
- 
-	if(msg.content === (p + "hi")) {
-		msg.channel.sendMessage("hi bitch");
-	}
+	
 });
 
 client.login(process.env.BOT_TOKEN);
