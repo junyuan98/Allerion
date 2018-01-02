d72627b6a3604accc770748ff67c43a2813fa555
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-02 20:36:27 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,30 +9,9 @@ client.on('ready', () => {
 const p = "a!";
 
 client.on('message', msg => {
-	if (msg.author.bot) return;
-	
-	if (msg.content === ( p + "checkversion" )) {
-		msg.channel.sendMessage("Allerion version 0.0.1 - Alpha-alpha phase");
-	}
-	
-	if (msg.content === ( p + "help" )) {
-		msg.channel.sendMessage("My prefix is a! and my available commands are:");
-		msg.channel.sendMessage("checkversion, checkavatar, random, randomquote, repeat, ping.");
-	}
-	
-	if (msg.content === (p + "random")) {
-		var x = randomnumber();
-		msg.channel.sendMessage("You have randomed " x );
-	}
-	
-	if (msg.content === (p + "checkavatar")) {
-		msg.reply(msg.author.avatarURL);
-	}
-	
-	if (msg.content === ( p + "ping" )) {
-		msg.channel.sendMessage( msg.author + ", Pong! My ping is " client.ping + "ms." );
-	}
-
+    if(msg.content === (p + "input")) {
+       msg.channel.sendMessage("output");
+    } 
 });
 
 client.login(process.env.BOT_TOKEN);
