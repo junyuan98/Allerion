1c9cc6766de023a3a27c6c87f5eaa00c6bbd4150
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-02 17:33:27 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,18 +17,22 @@ client.on('message', msg => {
   
   if(msg.content === (p + "trigger")) {
        msg.reply(' your mama so fat, when bounty hunter cast track on her, the team gain global vision.');
-}
+  }
   
   if(msg.content === (p + "info")) {
        msg.channel.sendMessage("My prefix is a! and my commands are: checkavatar, help, hi, trigger, ping, repeat");
-}
+  }
   
   if(msg.content === (p + "help")) {
        msg.channel.sendMessage("help your ass man im a prematured bot, go to info for more info");
-}
+  }
   
   if(msg.content === (p + "ping")) {
-    msg.reply('Pong!');
+    msg.chanel.sendMessage( msg.author + ", Pong!");
+    msg.chanel.sendMessage( client.ping + "ms");
+    if(suffix){
+      msg.channel.send( "note that !ping takes no arguments!");
+    }
   }
   
   
