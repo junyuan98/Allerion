46b835403c3206083cec9b61bbda7e5e86604df9
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-03 20:09:33 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ client.on('message', msg => {
 	if (msg.author.bot) return;
 	
 	if (msg.content === ( p + "version" )) { 
-		msg.channel.sendMessage("Allerion version A.0.0.7.9 - Triggerino");
+		msg.channel.sendMessage("Allerion version A.0.0.7.10 - Triggerino");
 		msg.channel.sendMessage("Trigger is in beta, selftrigger is removed (unless u all want it back) ");
 	}
 
