6da7a160d01f21d8ac3afa0aeb5ff5ca1e608de9
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-04 13:36:30 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const fs = require("fs");
+const points = require("points.json");
 const p = "a!";
 var ALLERIA = "331053004910362624";
 
