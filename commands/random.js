34ecd1672f80893601c8f533f45939386b902de2
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-06 17:28:16 +0800

    Update random.js

--- commands/random.js
+++ commands/random.js
@@ -1,1 +1,15 @@
+function randomnumber(y){
+	return [Math.floor(Math.random() * y + 1)];
+};
 
+exports.run = (client, msg, args) => {
+    var y = parseInt(args[0], 10);
+    if ( isNaN(y) === true )
+        msg.channel.sendMessage("Please enter a valid number");
+    else {
+			var x = randomnumber(y);
+			msg.channel.sendMessage("You have randomed " + x );
+    }
+}
+
+//args[0] = msg.content.substr("a!random ".length)
