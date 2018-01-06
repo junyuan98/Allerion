3eff0f35cbf50fa2e23ae2dd817a50d9c7e3bee2
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-06 16:17:36 +0800

    Update reload.js

--- commands/reload.js
+++ commands/reload.js
@@ -1,6 +1,10 @@
 exports.run = (client, msg, args) => {
-  if(!args || args.size < 1) return msg.reply("Must provide a command name to reload.");
-  // the path is relative to the *current folder*, so just ./filename.js
-  delete require.cache[require.resolve(`./${args[0]}.js`)];
-  msg.reply(`The command ${args[0]} has been reloaded`);
+    if (msg.author.id === ALLERIA){
+        if(!args || args.size < 1) return msg.reply("Must provide a command name to reload.");
+        // the path is relative to the *current folder*, so just ./filename.js
+        delete require.cache[require.resolve(`./${args[0]}.js`)];
+        msg.reply(`The command ${args[0]} has been reloaded`);
+    }
+    else
+        msg.reply('You have no permission to use this command');
 };
