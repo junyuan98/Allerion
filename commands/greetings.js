3cbd6efdfc231b9defdde01d6e9b86229fbedb3e
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-06 16:40:49 +0800

    Update greetings.js

--- commands/greetings.js
+++ commands/greetings.js
@@ -1,5 +1,5 @@
 exports.run = (client, msg, args) => {
-    if (msg.author.id === ALLERIA)
+    if (msg.author.id === "331053004910362624")
         msg.channel.sendMessage("Greetings owner <3 Have a nice day~");
     else
         msg.channel.sendMessage("Greetings Discorder " + msg.author.id + ", have a nice day");
