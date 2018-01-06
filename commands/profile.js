94de7e74c2bf5b7062ed820e469f6b4632c76021
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-06 16:25:15 +0800

    Update profile.js

--- commands/profile.js
+++ commands/profile.js
@@ -1,12 +1,15 @@
+
+let embed = new Discord.RichEmbed()
+.setAuthor(msg.author.username , msg.author.avatarURL)
+.setThumbnail(msg.author.avatarURL)
+.addField("Username", `${msg.author.tag}`)
+.addField("UserID", `${msg.author.id}`)
+.addField("Join server date", "Long long ago(_i guess_)")
+.setTimestamp()
+.setColor("#b200ff")
+.addBlankField(true);
+
+
 exports.run = (client, msg, args) => {
-		let embed = new Discord.RichEmbed()
-		.setAuthor(msg.author.username , msg.author.avatarURL)
-		.setThumbnail(msg.author.avatarURL)
-		.addField("Username", `${msg.author.tag}`)
-		.addField("UserID", `${msg.author.id}`)
-		.addField("Join server date", "Long long ago(_i guess_)")
-		.setTimestamp()
-		.setColor("#b200ff")
-		.addBlankField(true);
-		msg.channel.send({embed});
+		msg.channel.sendMessage({embed});
 }
