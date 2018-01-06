32028c64f35adc314dc1d4b09f80a974012b09ca
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-06 17:09:04 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,8 +21,8 @@ client.on('message', msg => {
 	const command = args.shift().toLowerCase();
 	
 	if (command === 'version'){ 
-		msg.channel.sendMessage("Allerion version A.0.0.10.22 - A Sensitive case");
-		msg.channel.sendMessage("```commands are now case insensitive (hooray)```");
+		msg.channel.sendMessage("Allerion version A.0.0.11.0 - LUL,a wild emoji appears");
+		msg.channel.sendMessage("```listemojis command is now available, profile malfunctioning```");
 	}
 
 	
@@ -60,7 +60,7 @@ client.on('message', msg => {
 		let embed = new Discord.RichEmbed()
 		.setAuthor("BOT Allerion" , client.user.avatarURL)
 		.setThumbnail(client.user.avatarURL)
-		.addField("**Commands**", "**ping**\n**avatar**\n**random**\n**version**\n**profile**\n**trigger**\n**greetings**\n**randomsing**")
+		.addField("**Commands**", "**ping**\n**avatar**\n**random**\n**version**\n**profile**\n**trigger**\n**greetings**\n**listemojis**\n**randomsing**")
 		.addField("**Character**","**level**")
 		.setFooter("Allerion")
 		.setTimestamp()
@@ -79,15 +79,12 @@ client.on('message', msg => {
 		else return;
 	}
 	
+	if (msg.content === "listemojis") {
+		const emojiList = msg.guild.emojis.map(e=>e.toString()).join(" ");
+		msg.channel.send(emojiList);
+	}
+	
 	/*
-	if (command === 'ping') {
-		msg.channel.sendMessage( msg.author + ", Pong! My ping is " + client.ping + "ms." );
-	}
-	
-	if (command === 'avatar') {
-		msg.reply(msg.author.avatarURL);
-	}
-	
 	if (command === 'random') {
 		var y = parseInt(msg.content.substr("random ".length + p.length), 10);
 		if ( isNaN(y) === true )
@@ -109,57 +106,13 @@ client.on('message', msg => {
 		.setColor("#b200ff")
 		.addBlankField(true);
 		msg.channel.send({embed});
-	}
-	
-	if (command === 'trigger') {
-		var textArray = [
-			'Fuck off',
-			'Fuck you',
-			'Shut up',
-			'Diam 7 pls',
-			'You are shit',
-			'Your momma so fat, when Bounty Hunter cast Track on her, the team gained global vision',
-			'Hi bitch'
-		];
-		var Quote = Math.floor(Math.random() * textArray.length);
-		msg.channel.sendMessage( msg.content.substr("trigger ".length + p.length) + ", " + `${textArray[Quote]}` ); 
-	}
-	
-	if (command === 'greetings') {
-		if (msg.author.id === ALLERIA)
-			msg.channel.sendMessage("Greetings owner <3 Have a nice day~");
-		else
-			msg.channel.sendMessage("Greetings Discorder " + msg.author.id + ", have a nice day");
-	}
-	
-	if (command === 'randomsing') {
-		var songLyrics = [
-			'Havana oh nana, half of my heart is in havana na na na',
-			'Is it too late now to say sorry',
-			'2+2 IS 4, MINUS 1 THATS 3 QUICK MATHS',
-			'BOOM BOOM POW GONNA GET GET',
-			'Despacito, nlskdnflkjkljfklsrfkljdlk burito',
-			'You just want attention, you dont want my heart',
-			'We were just kids when we fell , in, love',
-			'People fall in love in mysterious ways, maybe just the touch of a hand',
-			'你好吗,我会中文 Ching Chong',
-			'AND IIIIIIIIIII***IIII***IIII WILL ALWAYSSSSS LOVEEE YOUUUUU_UUU_**UUU**UUUuuu',
-			'When I see your face, theres not a thing that I would change, cause youre amazing, just the way you are',
-			'Gave you all I had and you tossed it in the trash, you tossed it in the trash, you did',
-			'I hear Jerusalem bells a-ringing, Roman Cavalry choirs are singing',
-			'Duuuuuuuuuuuuuuuuuuuuuuun, Dun dun dun dun dun dun dun dun dun dun dun dundun dun dundundun dun dun dun dun dun dun dundun dundun, BOOM',
-			'Im a barbie girl, in a barbie world',
-		];
-		var number = Math.floor(Math.random() * songLyrics.length);
-		msg.channel.sendMessage(`${songLyrics[number]}`);
-	}
-	
+	}*/
 	//Beta commands
 	
 	if (command === 'test') {
 		let [age, sex, location] = args;
 		msg.reply(`Hello ${msg.author.username}, I see you're a ${age} year old ${sex} from ${location}. Wanna date?`);
-	}*/
+	}
 
 	//Tags people if know id -> msg.channel.sendMessage("<@" + msg.author.id +">");
 });
