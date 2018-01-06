4f44955fcd10d9af516ac61010d0d10cc132c598
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-06 16:40:03 +0800

    Update trigger.js

--- commands/trigger.js
+++ commands/trigger.js
@@ -1,1 +1,15 @@
+var textArray = [
+  'Fuck off',
+  'Fuck you',
+  'Shut up',
+  'Diam 7 pls',
+  'You are shit',
+  'Your momma so fat, when Bounty Hunter cast Track on her, the team gained global vision',
+  'Hi bitch'
+];
 
+exports.run = (client, msg, args) => {
+  var Quote = Math.floor(Math.random() * textArray.length);
+  msg.channel.sendMessage( msg.content.substr("trigger ".length + p.length) + ", " + `${textArray[Quote]}` ); 
+	}
+}
