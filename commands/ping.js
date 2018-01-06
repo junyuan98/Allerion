86aeeb7d4f75a7197f5f4dd79453db0111fab9ea
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-06 16:00:59 +0800

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,3 +1,3 @@
 exports.run = (client, msg, args) => {
-    msg.channel.sendMessage( msg.author + ", Pong! My ping is " + client.ping + "ms." ).catch(console.error);
+    msg.channel.sendMessage( msg.author + ", Ping Pong! My ping is " + client.ping + "ms." ).catch(console.error);
 }
