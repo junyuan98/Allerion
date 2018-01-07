1bbd42d6c834a0f541fb4f2f4f046d39bb00e42f
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-07 15:32:35 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,7 +26,7 @@ client.on('message', msg => {
 		}
 		
 		if ( command === "version"){ 
-			msg.channel.sendMessage("Allerion version A.0.0.13.12 - Low Profile");
+			msg.channel.sendMessage("Allerion version A.0.0.13.13 - Low Profile");
 			msg.channel.sendMessage("`New command suggestions are welcomed`");
 		}
 	
