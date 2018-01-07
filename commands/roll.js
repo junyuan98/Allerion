bab216626f61bcabd993de332469a6c7698f0e0a
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-07 15:43:20 +0800

    Update roll.js

--- commands/roll.js
+++ commands/roll.js
@@ -1,1 +1,5 @@
-
+exports.run = (client, msg, args) => {
+        var number = Math.floor(Math.random() * args.length);
+        msg.channel.sendMessage("You have rolled " + `${args[number]}` + "!");
+}
+        
