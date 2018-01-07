8d9b1a5ad1f980d155b05d819f09693f884ba3f4
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-07 12:35:24 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,27 +22,12 @@ client.on('message', msg => {
 
 	}
 	
-	const swearWords = ["fuck", "cb", "sohai", "noob"];
-	if( swearWords.some(word => msg.content.toLowerCase().includes(word)) ) {
-		msg.channel.sendMessage("Dont rude la cb");
-	}
-	
-	const terms = ["ExtremeDie", "ED"];
-	if( terms.some(word => msg.content.includes(word)) ) {
-		msg.channel.sendMessage("ED is GOD, ED is LIFE");
-	}
-	
-	const extreme = ["extreme"];
-	if( extreme.some(word => msg.content.toLowerCase().includes(word)) ) {
-		msg.channel.sendMessage("GIFF EXTREME OR RIOT");
-	}
-	
 	if (msg.content.startsWith(prefix)){
 		const args = msg.content.slice(prefix.length).trim().split(/ +/g);
 		const command = args.shift().toLowerCase();
 	
 		if ( command === "version"){ 
-			msg.channel.sendMessage("Allerion version A.0.0.12.21 - Tag and run");
+			msg.channel.sendMessage("Allerion version A.0.0.12.25 - Tag and run");
 			msg.channel.sendMessage("```TAGGING HAS SUCCEED, profile _STILL_ malfunctioning```");
 		}
 
@@ -99,6 +84,22 @@ client.on('message', msg => {
 			msg.reply(`Hello ${msg.author.username}, I see you're a ${age} year old ${sex} from ${location}. Wanna date?`);
 		}
 	}
+	
+	const swearWords = ["fuck", "cb", "sohai", "noob"];
+	if( swearWords.some(word => msg.content.toLowerCase().includes(word)) ) {
+		msg.channel.sendMessage("Dont rude la cb");
+	}
+	
+	const terms = ["ExtremeDie", "ED"];
+	if( terms.some(word => msg.content.includes(word)) ) {
+		msg.channel.sendMessage("ED is GOD, ED is LIFE");
+	}
+	
+	const extreme = ["extreme"];
+	if( extreme.some(word => msg.content.toLowerCase().includes(word)) ) {
+		msg.channel.sendMessage("GIFF EXTREME OR RIOT");
+	}
+	
 	//Tags people if know id -> msg.channel.sendMessage("<@!" + msg.author.id +">");
 });
 
