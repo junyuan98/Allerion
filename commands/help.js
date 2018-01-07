2a2c75b51221fd2b5804c35cbcbcea5faa157a58
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-07 21:31:47 +0800

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -6,21 +6,21 @@ exports.run = (client, msg, args) => {
 	.setThumbnail(client.user.avatarURL)
 	.setDescription(`
 		**__Available Commands__**
-	help          \t- You just typed this
-	ping          \t- Plays ping pong with you
-	roll          \t- Rolls between variables you typed (space between variables)
-	avatar        \t- Displays your own avatar (link included)
-	random        \t- Type a number behind and randoms a number
-	trigger       \t- Tag a person after the command to have fun
-	profile       \t- Displays your profile (still improving)
-	version       \t- Shows the current version of the BOT
-	listemojis    \t- Show all the custom emojis available in current server
-	greetings     \t- Need some love? type this
-	randomsing    \t- Sing random lyrics for you
-	\n**__Future Commands__**
-	level         \t- Display levels for you
-	quoteoftheday \t- Not sure why this is here
-		`)
+help   
+ping      
+roll         
+avatar     
+random        
+trigger       
+profile       
+version       
+listemojis    
+greetings     
+randomsing    
+**__Future Commands__**
+level         
+quoteoftheday 
+	`)
 	.setFooter("Allerion")
 	.setTimestamp()
 	.setColor("#000000")
