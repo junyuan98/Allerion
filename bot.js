4c067f5f54311a5e0567f44ccfcafaf599715626
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-07 12:22:15 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,22 +13,6 @@ client.on('ready', () => {
 client.on('message', msg => {
 	if (msg.author.bot) return;
 	if (msg.channel.type === "dm") return; // Ignore DM channels.
-	if (!msg.content.startsWith(prefix)) return;
-	
-	const args = msg.content.slice(prefix.length).trim().split(/ +/g);
-	const command = args.shift().toLowerCase();
-	
-	if ( command === "version"){ 
-		msg.channel.sendMessage("Allerion version A.0.0.12.20 - Tag and run");
-		msg.channel.sendMessage("```TAGGING HAS SUCCEED, profile _STILL_ malfunctioning```");
-	}
-
-	/*try {
-		let commandFile = require(`./commands/${command}.js`);
-		commandFile.run(client, msg, args);
-	} catch (err) {
-		console.error(err);
-	}
 	
 	const LOLs = ["LUL", "HAHA"];
 	if( LOLs.some(word => msg.content.includes(word)) ) {
@@ -53,54 +37,69 @@ client.on('message', msg => {
 		msg.channel.sendMessage("GIFF EXTREME OR RIOT");
 	}
 	
-	if (command === 'help') {
-		let embed = new Discord.RichEmbed()
-		.setAuthor("BOT Allerion" , client.user.avatarURL)
-		.setThumbnail(client.user.avatarURL)
-		.addField("**Commands**", "**ping**\n**avatar**\n**random**\n**version**\n**profile**\n**trigger**\n**greetings**\n**listemojis**\n**randomsing**")
-		.addField("**Character**","**level**")
-		.setFooter("Allerion")
-		.setTimestamp()
-		.setColor("#000000")
-		.addBlankField(true);
-		msg.channel.send({embed});
-	}
+	if (msg.content.startsWith(prefix)){
+		const args = msg.content.slice(prefix.length).trim().split(/ +/g);
+		const command = args.shift().toLowerCase();
 	
-	if (command === 'kick') {
-		if (msg.author.id === ALLERIA) {
-			let member = msg.mentions.members.first();
-			member.kick();
-			msg.channel.sendMessage(`${member.user.username} was succesfully kicked. Hooray! (or nay)`);
+		if ( command === "version"){ 
+			msg.channel.sendMessage("Allerion version A.0.0.12.21 - Tag and run");
+			msg.channel.sendMessage("```TAGGING HAS SUCCEED, profile _STILL_ malfunctioning```");
 		}
-	}
-	
-	if (command === 'tag') {
-		let member = msg.mentions.members.first();
-		msg.channel.sendMessage(`${member.id}`);
-		msg.channel.sendMessage("<@!" + `${member.id}` +"> , Life is nice");
-		
-	}
 
-	if (command === 'profile') {
-		let embed = new Discord.RichEmbed()
-		.setAuthor(msg.author.username , msg.author.avatarURL)
-		.setThumbnail(msg.author.avatarURL)
-		.addField("Username", `${msg.author.tag}`)
-		.addField("UserID", `${msg.author.id}`)
-		.addField("Join server date", "Long long ago(_i guess_)")
-		.setTimestamp()
-		.setColor("#b200ff")
-		.addBlankField(true);
-		msg.channel.send({embed});
-	}
+		try {
+			let commandFile = require(`./commands/${command}.js`);
+			commandFile.run(client, msg, args);
+		} catch (err) {
+			console.error(err);
+		}
+	
+		if (command === 'help') {
+			let embed = new Discord.RichEmbed()
+			.setAuthor("BOT Allerion" , client.user.avatarURL)
+			.setThumbnail(client.user.avatarURL)
+			.addField("**Commands**", "**ping**\n**avatar**\n**random**\n**version**\n**profile**\n**trigger**\n**greetings**\n**listemojis**\n**randomsing**")
+			.addField("**Character**","**level**")
+			.setFooter("Allerion")
+			.setTimestamp()
+			.setColor("#000000")
+			.addBlankField(true);
+			msg.channel.send({embed});
+		}
+	
+		if (command === 'kick') {
+			if (msg.author.id === ALLERIA) {
+				let member = msg.mentions.members.first();
+				member.kick();
+				msg.channel.sendMessage(`${member.user.username} was succesfully kicked. Hooray! (or nay)`);
+			}
+		}
+	
+		if (command === 'tag') {
+			let member = msg.mentions.members.first();
+			msg.channel.sendMessage(`${member.id}`);
+			msg.channel.sendMessage("<@!" + `${member.id}` +"> , Life is nice");
+		}
+
+		/*if (command === 'profile') {
+			let embed = new Discord.RichEmbed()
+			.setAuthor(msg.author.username , msg.author.avatarURL)
+			.setThumbnail(msg.author.avatarURL)
+			.addField("Username", `${msg.author.tag}`)
+			.addField("UserID", `${msg.author.id}`)
+			.addField("Join server date", "Long long ago(_i guess_)")
+			.setTimestamp()
+			.setColor("#b200ff")
+			.addBlankField(true);
+			msg.channel.send({embed});
+		}*/
 	//Beta commands
 	
-	if (command === 'test') {
-		let [age, sex, location] = args;
-		msg.reply(`Hello ${msg.author.username}, I see you're a ${age} year old ${sex} from ${location}. Wanna date?`);
-	}*/
-
-	//Tags people if know id -> msg.channel.sendMessage("<@" + msg.author.id +">");
+		if (command === 'test') {
+			let [age, sex, location] = args;
+			msg.reply(`Hello ${msg.author.username}, I see you're a ${age} year old ${sex} from ${location}. Wanna date?`);
+		}
+	}
+	//Tags people if know id -> msg.channel.sendMessage("<@!" + msg.author.id +">");
 });
 
 client.login(process.env.BOT_TOKEN);
