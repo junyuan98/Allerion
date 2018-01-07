c789b510adf8b77d23e179e0b8f9ff9a022f1e17
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-07 16:50:26 +0800

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,14 +1,25 @@
 const Discord = require("discord.js");
 
 exports.run = (client, msg, args) => {
-	const helplist = new Discord.RichEmbed()
-	.setAuthor("BOT Allerion" , client.user.avatarURL)
+	const embed = new Discord.RichEmbed()
+	.setAuthor("text", "https://bestsecuritysearch.com/wp-content/uploads/2016/10/discord-logo-bss.png")
+	.setThumbnail("https://image.ibb.co/dkT4yR/XD.png")
+	.addField("field", "text below field", true)
+	.addField("field2", "text below field2", true)
+	.setImage("https://image.ibb.co/dkT4yR/XD.png")
+	.setFooter("text")
+	.setTimestamp()
+	.setColor("#b200ff")
+	.addBlankField(true)
+	msg.channel.send(embed);
+	/*.setAuthor("BOT Allerion" , client.user.avatarURL)
 	.setThumbnail(client.user.avatarURL)
 	.addField("**Commands**", "**ping**\n**roll**\n**avatar**\n**random**\n**version**\n**profile**\n**trigger**\n**greetings**\n**listemojis**\n**randomsing**")
 	.addField("**Character**","**level**")
 	.setFooter("Allerion")
 	.setTimestamp()
 	.setColor("#000000")
-	.addBlankField(true);
-	msg.channel.send({helplist});
+	.addBlankField(true);*/
+	
+	msg.channel.send({embed});
 }
