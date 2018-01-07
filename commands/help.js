93e673ed2ecef8103a7e1ad50d61fbe91cc8f49f
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-07 16:56:10 +0800

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,8 +2,9 @@ const Discord = require("discord.js");
 
 exports.run = (client, msg, args) => {
 	const embed = new Discord.RichEmbed()
-	.setAuthor("BOT Allerion")
-	.addField("field", "text below field")
+	.setAuthor("BOT Allerion", client.user.avatarURL)
+	.setThumbnail(client.user.avatarURL)
+	.addField("**Commands**", "**ping**\n**roll**\n**avatar**\n**random**\n**version**\n**profile**\n**trigger**\n**greetings**\n**listemojis**\n**randomsing**")
 	.setFooter("Allerion")
 	.setTimestamp()
 	.setColor("#000000")
