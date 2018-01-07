059f698c4d492e81d911d82027bf5732dcb976f8
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-07 16:10:35 +0800

    Update roll.js

--- commands/roll.js
+++ commands/roll.js
@@ -1,5 +1,17 @@
+
 exports.run = (client, msg, args) => {
         var number = Math.floor(Math.random() * args.length);
         msg.channel.sendMessage("You have rolled " + `${args[number]}` + "!");
+        
+        const embed = new Discord.RichEmbed()
+        .setAuthor("ALLERION ROLLS", client.user.avatarURL)   
+        .setColor(0x00AE86)
+        .setDescription("You have rolled " + `${args[number]}` + "!")
+        .setFooter("Gambling is bad")
+        .setTimestamp()
+        .addBlankField(true);
+        
+        msg.channel.send({embed});
+        
 }
         
