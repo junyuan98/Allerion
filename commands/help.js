6721b48d9602c0ddaa1469e64023cced19357648
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-07 16:34:57 +0800

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,1 +1,14 @@
+const Discord = require("discord.js");
 
+exports.run = (client, msg, args) => {
+        let helplist = new Discord.RichEmbed()
+	.setAuthor("BOT Allerion" , client.user.avatarURL)
+	.setThumbnail(client.user.avatarURL)
+	.addField("**Commands**", "**ping**\n**roll**\n**avatar**\n**random**\n**version**\n**profile**\n**trigger**\n**greetings**\n**listemojis**\n**randomsing**")
+	.addField("**Character**","**level**")
+	.setFooter("Allerion")
+	.setTimestamp()
+	.setColor("#000000")
+	.addBlankField(true);
+	msg.channel.send({helplist});
+}
