ca52c52d9dc44f452fc67388e429db3d192f696b
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-07 20:57:14 +0800

    Update roll.js

--- commands/roll.js
+++ commands/roll.js
@@ -1,15 +1,19 @@
 const Discord = require("discord.js");
 
 exports.run = (client, msg, args) => {
-        var number = Math.floor(Math.random() * args.length);
+        if (args === undefined) {
+                 msg.channel.sendMessage("Please state your term that you want to roll, etc. a!roll A B C");
+        }
+        else {
+                var number = Math.floor(Math.random() * args.length);
         
-        const embed = new Discord.RichEmbed()
-        .setAuthor("ALLERION ROLLS", client.user.avatarURL)   
-        .setColor(0x00AE86)
-        .setDescription("You have rolled " + `${args[number]}` + "!")
-        .setFooter("Gambling is bad")
-        .setTimestamp()
-        
-        msg.channel.send({embed});
+                const embed = new Discord.RichEmbed()
+                .setAuthor("ALLERION ROLLS", client.user.avatarURL)   
+                .setColor(0x00AE86)
+                .setDescription("You have rolled " + `${args[number]}` + "!")
+                .setFooter("Gambling is bad")
+                .setTimestamp()
+                msg.channel.send({embed});
+        }
 }
         
