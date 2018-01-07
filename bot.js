ff03774f490d5f54338e0fe612db0268d95ec4ed
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-07 17:12:21 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,20 +12,8 @@ client.on('ready', () => {
 });
 
 client.on("guildMemberAdd", (member) => {
-	const guild = member.guild;
-	if (!newUsers[guild.id]) newUsers[guild.id] = new Discord.Collection();
-	newUsers[guild.id].set(member.id, member.user);
-
-	if (newUsers[guild.id].size > 10) {
-		const userlist = newUsers[guild.id].map(u => u.toString()).join(" ");
-		guild.channels.get(guild.id).send("Welcome our new users!\n" + userlist);
-		newUsers[guild.id].clear();
-	}
-});
-
-client.on("guildMemberRemove", (member) => {
-	const guild = member.guild;
-	if (newUsers[guild.id].has(member.id)) newUsers.delete(member.id);
+	console.log(`Our new member "${member.user.username}" has joined "${member.guild.name}"` );
+	member.guild.channels.get("welcome").send(`"${member.user.username}" has joined this server`);
 });
 
 client.on('message', msg => {
