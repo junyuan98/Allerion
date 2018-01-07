e3e3a6c5214208fef4db15ebe03d28cfb3e9caff
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-07 16:20:19 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,7 +26,7 @@ client.on('message', msg => {
 		}
 		
 		if ( command === "version"){ 
-			msg.channel.sendMessage("Allerion version A.0.0.14.1 - Rolling in the deep");
+			msg.channel.sendMessage("Allerion version A.0.0.14.5 - Rolling in the deep");
 			msg.channel.sendMessage("`Roll is in construction, New command suggestions are welcomed`");
 		}
 	
@@ -41,24 +41,6 @@ client.on('message', msg => {
 			.setColor("#000000")
 			.addBlankField(true);
 			msg.channel.send({newembed});
-		}
-		
-		if (command === 'profile') {
-			let member = msg.mentions.members.first();
-			if (!member){
-				msg.channel.sendMessage("Please tag a vaild member");
-			}
-			else {
-				let embed = new Discord.RichEmbed()
-				.setAuthor(member.user.username , member.user.avatarURL)
-				.setThumbnail(member.user.avatarURL)
-				.addField("Username", `${member.user.tag}`)
-				.addField("UserID", `${member.user.id}`)
-				.addField("Join server date", "Long long ago(_i guess_)")
-				.setTimestamp()
-				.setColor("#b200ff");
-				msg.channel.send({embed})
-			}
 		}
 		
 		if (command === 'tag') {
