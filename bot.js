51a1d932dfd8e175ded87f7ffe4b07250383a03b
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-07 15:14:02 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -45,16 +45,28 @@ client.on('message', msg => {
 		
 		if (command === 'profile') {
 			let member = msg.mentions.members.first();
-			
-			if (!member){
-				msg.channel.sendMessage("Please tag a vaild member");
+			if (member !== ""){
+				if (!member){
+					msg.channel.sendMessage("Please tag a vaild member");
+				}
+				else {
+					let embed = new Discord.RichEmbed()
+					.setAuthor(member.user.username , member.user.avatarURL)
+					.setThumbnail(member.user.avatarURL)
+					.addField("Username", `${member.user.tag}`)
+					.addField("UserID", `${member.user.id}`)
+					.addField("Join server date", "Long long ago(_i guess_)")
+					.setTimestamp()
+					.setColor("#b200ff");
+					msg.channel.send({embed});
+				}
 			}
 			else {
 				let embed = new Discord.RichEmbed()
-				.setAuthor(member.user.username , member.user.avatarURL)
-				.setThumbnail(member.user.avatarURL)
-				.addField("Username", `${member.user.tag}`)
-				.addField("UserID", `${member.user.id}`)
+				.setAuthor(msg.author.username , msg.author.avatarURL)
+				.setThumbnail(msg.author.avatarURL)
+				.addField("Username", `${msg.author.tag}`)
+				.addField("UserID", `${msg.author.id}`)
 				.addField("Join server date", "Long long ago(_i guess_)")
 				.setTimestamp()
 				.setColor("#b200ff");
