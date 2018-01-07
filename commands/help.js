c7fa57df1f294aa822952768d475c6ee795b5263
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-07 21:19:44 +0800

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -6,19 +6,20 @@ exports.run = (client, msg, args) => {
 	.setThumbnail(client.user.avatarURL)
 	.setDescription(`
 		**__Available Commands__**
-		help\t\t- You just typed this
-		ping\t\t- Plays ping pong with you
-		roll\t\t- Rolls between variables you typed (space between variables)
-		avatar\t\t- Displays your own avatar (link included)
-		random\t\t- Type a number behind and randoms a number
-		trigger\t- Tag a person after the command to have fun
-		profile\t- Displays your profile (still improving)
-		listemojis\t- Show all the emojis available in current server
-		greetings\t- Need some love? type this
-		randomsing\t- Sing random lyrics for you
-		**__Future Commands__**
-		level\t\t\t- Display levels for you
-		quoteoftheday\t\t- Not sure why this is here
+	help\t\t- You just typed this
+	ping\t\t- Plays ping pong with you
+	roll\t\t- Rolls between variables you typed (space between variables)
+	avatar\t\t- Displays your own avatar (link included)
+	random\t\t- Type a number behind and randoms a number
+	trigger\t- Tag a person after the command to have fun
+	profile\t- Displays your profile (still improving)
+	version\t- Shows the current version of the BOT
+	listemojis\t- Show all the custom emojis available in current server
+	greetings\t- Need some love? type this
+	randomsing\t- Sing random lyrics for you
+	**__Future Commands__**
+	level\t\t\t- Display levels for you
+	quoteoftheday\t\t- Not sure why this is here
 		`)
 	.setFooter("Allerion")
 	.setTimestamp()
