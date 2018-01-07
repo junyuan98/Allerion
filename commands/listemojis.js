88e08d02c814e46659690b5e8c4d27f8a970f3dd
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-07 21:36:09 +0800

    Update listemojis.js

--- commands/listemojis.js
+++ commands/listemojis.js
@@ -1,4 +1,4 @@
 exports.run = (client, msg, args) => {
-    const emojiList = msg.guild.emojis.map(e=>e.toString()).join(" ");
+    const emojiList = msg.guild.emojis.map(e=>e.toString()).join(" test ");
     msg.channel.send(emojiList);
 }
