de406d16a939f04c57ed06e7b5201924ab5f6e57
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-07 17:42:39 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,6 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const fs = require("fs");
 const prefix = "a!";
-const newUsers = [];
 
 var ALLERIA = "331053004910362624";
 
