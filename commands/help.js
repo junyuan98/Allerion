421069b06a705802a89ade88b83b63e48d71044b
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-07 21:13:32 +0800

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -4,20 +4,21 @@ exports.run = (client, msg, args) => {
 	const embed = new Discord.RichEmbed()
 	.setAuthor("BOT Allerion", client.user.avatarURL)
 	.setThumbnail(client.user.avatarURL)
-	..setDescription(`
+	.setDescription(`
 		**__Available Commands__**
-		${prefix}help 		- You just typed this
-		${prefix}ping 		- Plays ping pong with you
-		${prefix}roll 		- Rolls between variables you typed (space between variables)
-		${prefix}avatar 	- Displays your own avatar (link included)
-		${prefix}random 	- Type a number behind and randoms a number
-		${prefix}trigger 	- Tag a person after the command to have fun
-		${prefix}profile 	- Displays your profile (still improving)
-		${prefix}listemojis 	- Show all the emojis available in current server
-		${prefix}greetings 	- Need some love? type this
-		${prefix}randomsing 	- Sing random lyrics for you
+		help 		- You just typed this
+		ping 		- Plays ping pong with you
+		roll 		- Rolls between variables you typed (space between variables)
+		avatar 		- Displays your own avatar (link included)
+		random 		- Type a number behind and randoms a number
+		trigger 	- Tag a person after the command to have fun
+		profile 	- Displays your profile (still improving)
+		listemojis 	- Show all the emojis available in current server
+		greetings 	- Need some love? type this
+		randomsing 	- Sing random lyrics for you
 		**__Future Commands__**
-		${prefix}level
+		level		- Display levels for you
+		quoteoftheday	- Not sure why this is here
 		`)
 	.setFooter("Allerion")
 	.setTimestamp()
