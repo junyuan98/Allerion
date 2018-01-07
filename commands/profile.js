7ed0170668330231d8f7dc2364b3dc17b8056575
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-07 16:20:23 +0800

    Update profile.js

--- commands/profile.js
+++ commands/profile.js
@@ -1,1 +1,19 @@
 const Discord = require("discord.js");
+
+exports.run = (client, msg, args) => {
+        let member = msg.mentions.members.first();
+        if (!member){
+                msg.channel.sendMessage("Please tag a vaild member");
+        }
+        else {
+                let embed = new Discord.RichEmbed()
+                .setAuthor(member.user.username , member.user.avatarURL)
+                .setThumbnail(member.user.avatarURL)				
+                .addField("Username", `${member.user.tag}`)
+		.addField("UserID", `${member.user.id}`)
+		.addField("Join server date", "Long long ago(_i guess_)")
+		.setTimestamp()
+		.setColor("#b200ff");
+                msg.channel.send({embed})
+        }
+}
