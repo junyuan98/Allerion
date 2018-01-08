7e778b6c102d24b05fd380cf6911b250975382ca
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-08 18:18:31 +0800

    Update points.js

--- commands/points.js
+++ commands/points.js
@@ -1,4 +1,4 @@
-exports.run = (client, msg, args) => {
+exports.run = async (client, msg, args) => {
 	const scorePoints = client.points.get(msg.author.id).points;
 	!scorePoints ? msg.channel.send('You have no points yet.') : msg.channel.send(`You have ${scorePoints} points!`);
 }
