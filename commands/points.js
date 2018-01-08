d09e239e92146c30bdf09ec0b252d5a940b4f878
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-09 01:22:44 +0800

    Delete points.js

--- commands/points.js
+++
@@ -1,6 +0,0 @@
-exports.run = async (client, msg, args, sql) => {
-	sql.get(`SELECT * FROM scores WHERE userId ="${msg.author.id}"`).then(row => {
-		if (!row) return message.reply("sadly you do not have any points yet!");
-		msg.reply(`you currently have ${row.points} points, good going!`);
-    });
-}
