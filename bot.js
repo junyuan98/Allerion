2c391e9a349d9a1f8e0a2ed41f67b0a8cba40383
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-08 14:59:11 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const fs = require("fs");
+const Enmap = require("enmap");
+const EnmapLevel = require("enmap-level");
 const prefix = "a!";
 var ALLERIA = "331053004910362624";
 
