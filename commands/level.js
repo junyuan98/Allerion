7df81bcc56fd40632ac176eac618ce60ec316ea1
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-09 01:22:47 +0800

    Delete level.js

--- commands/level.js
+++
@@ -1,6 +0,0 @@
-exports.run = async (client, msg, args, sql) => {
-        sql.get(`SELECT * FROM scores WHERE userId ="${msg.author.id}"`).then(row => {
-                if (!row) return msg.reply("Your current level is 0");
-                msg.reply(`Your current level is ${row.level}`);
-        });
-}
