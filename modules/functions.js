6811a355bad26d86011d9ae8c95f72669446ea04
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-08 18:14:08 +0800

    Update functions.js

--- modules/functions.js
+++ modules/functions.js
@@ -2,7 +2,7 @@ module.exports = (client) => {
         client.pointsMonitor = (client, msg) => {
                 if (msg.channel.type !=='text') return;
                 const settings = client.settings.get(msg.guild.id);
-                if (msg.content.startsWith(settings.prefix)) return;
+                //if (msg.content.startsWith(settings.prefix)) return;
                 const score = client.points.get(msg.author.id) || { points: 0, level: 0 };
                 score.points++;
                 const curLevel = Math.floor(0.5 * Math.sqrt(score.points));
