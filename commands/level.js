42ab5167775240397a8c5f2a5c78e466b4781941
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-08 15:48:08 +0800

    Update level.js

--- commands/level.js
+++ commands/level.js
@@ -1,4 +1,4 @@
 exports.run = (client, msg, args) => {
-  const scoreLevel = client.points.get(message.author.id).level;
-  !scoreLevel ? message.channel.send('You have no levels yet.') : message.channel.send(`You are currently level ${scoreLevel}!`);
+  const scoreLevel = client.points.get(msg.author.id).level;
+  !scoreLevel ? msg.channel.send('You have no levels yet.') : msg.channel.send(`You are currently level ${scoreLevel}!`);
 }
