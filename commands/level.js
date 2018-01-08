cc9eb0b264f12f702fd6c4b255d41dbcfbdcfc19
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-08 14:14:01 +0800

    Update level.js

--- commands/level.js
+++ commands/level.js
@@ -1,3 +1,3 @@
-exports.run = (client, msg, args) => {
+exports.run = (client, msg, args, userData) => {
 		msg.reply(`You are currently level ${userData.level}, with ${userData.points} points.`);
 }
