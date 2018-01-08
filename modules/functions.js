21f1719f92d2bcbfb0d8abd6388a89730b5d914a
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-08 18:10:26 +0800

    Update functions.js

--- modules/functions.js
+++ modules/functions.js
@@ -1,13 +1,15 @@
-client.pointsMonitor = (client, msg) => {
-        if (msg.channel.type !=='text') return;
-        const settings = client.settings.get(msg.guild.id);
-        if (msg.content.startsWith(settings.prefix)) return;
-        const score = client.points.get(msg.author.id) || { points: 0, level: 0 };
-        score.points++;
-        const curLevel = Math.floor(0.5 * Math.sqrt(score.points));
-        if (score.level < curLevel) {
-                msg.reply(`You've leveled up to level **${curLevel}**!`);
-                score.level = curLevel;
-        }
-        client.points.set(msg.author.id, score);
+module.exports = (client) => {
+        client.pointsMonitor = (client, msg) => {
+                if (msg.channel.type !=='text') return;
+                const settings = client.settings.get(msg.guild.id);
+                if (msg.content.startsWith(settings.prefix)) return;
+                const score = client.points.get(msg.author.id) || { points: 0, level: 0 };
+                score.points++;
+                const curLevel = Math.floor(0.5 * Math.sqrt(score.points));
+                if (score.level < curLevel) {
+                        msg.reply(`You've leveled up to level **${curLevel}**!`);
+                        score.level = curLevel;
+                }
+                client.points.set(msg.author.id, score);
+        };
 };
