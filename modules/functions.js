e67a4236aa595f2abb44bd2ed8c501918b3f0f3e
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2018-01-08 15:28:38 +0800

    Update functions.js

--- modules/functions.js
+++ modules/functions.js
@@ -1,3 +1,13 @@
 client.pointsMonitor = (client, message) => {
-
+  if (message.channel.type !=='text') return;
+  const settings = client.settings.get(message.guild.id);
+  if (message.content.startsWith(settings.prefix)) return;
+  const score = client.points.get(message.author.id) || { points: 0, level: 0 };
+  score.points++;
+  const curLevel = Math.floor(0.1 * Math.sqrt(score.points));
+  if (score.level < curLevel) {
+    message.reply(`You've leveled up to level **${curLevel}**! Ain't that dandy?`);
+    score.level = curLevel;
+  }
+  client.points.set(message.author.id, score);
 };
