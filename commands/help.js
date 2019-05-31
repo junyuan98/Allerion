2b687553b93727190118d3ba8df0a16a2a25f63c
junyuan98 <35001401+junyuan98@users.noreply.github.com>
2019-05-31 23:13:31 +0800

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,30 +1,71 @@
 const Discord = require("discord.js");
 
-exports.run = (client, msg, args) => {
-	const embed = new Discord.RichEmbed()
-	.setAuthor("BOT Allerion", client.user.avatarURL)
-	.setThumbnail(client.user.avatarURL)
-	.setDescription(`
-**__Available Commands__**
-help   
+exports.run = async (client, msg, args) => {
+	if (!args[0]){
+		const embed = new Discord.RichEmbed()
+		.setAuthor("BOT Allerion")
+		.setDescription(`
+**Allerion version A.1.0.0.0 - RESURRECTED**
+BIGJACKPOT MODE AND RAFFLE(FOR GIVEAWAYS) WILL BE RELEASED SOON!! STAY TUNED!!
+NEW HELP FUNCTION, Type a!help [command] to get specific details of the command.
+JACKPOT MODE RELEASED!!! 1 RAFFLETICKET TO PARTICIPATE AND WIN THE PRIZEPOT!!!
+	`)
+		.setThumbnail(client.user.avatarURL)
+		.addField("**__General Commands__**",`  
 ping      
 roll         
 avatar     
 random        
-trigger       
-profile       
-version       
-listemojis    
-greetings     
-randomsing    
+trigger	
+randomsing `,true)
+		.addField("**__Profile Commands__**",`
+**NEW** : buy
+shop
+ranks
+daily
+level
+stats
+profile 
+**NEW** : scoreboard
+upgradeclass
+		`,true)
+		.addField("**__The Casino__**",`
+***IN BETA***: dicegame
+***NEW***: jackpot
+***SOON***: raffle
+***SOON***: bigjackpot
+		`,true)
+		.addField("**Special Commands**",`
+		event
+				`,true)
+		.setFooter("Allerion")
+		.setTimestamp()
+		.setColor("#ffffff");
+		msg.channel.send({embed});
+	}
+	else {
+		let command = args[0];
+		if (client.commands.has(command)){
+			command = client.commands.get(command);
+			const embed = new Discord.RichEmbed()
+			.setAuthor(`a!${command.help.name}`)
+			.setDescription(`
+Description: ${command.help.description}
+Usage: ${command.help.usage}
+			`)
+			.setFooter("Allerion")			
+			.setTimestamp()
+			.setColor("#ffffff");
+			msg.channel.send({embed});
+		}
+		else {
+			msg.channel.send('There is no such command');
+		}
+	}
+};
 
-**__Future Commands__**
-level         
-quoteoftheday 
-	`)
-	.setFooter("Allerion")
-	.setTimestamp()
-	.setColor("#000000")
-	.addBlankField(true);
-	msg.channel.send({embed});
-}
+exports.help = {
+	name: "help",
+	description: "Help you list all the commands of course.",
+	usage: "`a!help` or `a!help [commands]`"
+};
